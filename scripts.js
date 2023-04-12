d898d03f63c531334d85e8570b840a74a3cb42be
caioramirodev <caioramirodev@gmail.com>
2023-04-12 20:13:43 -0300

    CommitJS2

--- scripts.js
+++ scripts.js
@@ -1,13 +1,23 @@
 
-const button = document.getElementById ('convert-button')
+const button = document.getElementById('convert-button')
 
 const dolar = 5.2
 
 
 const convertValues = () => {
     const inputReais = document.getElementById('input-real').value
+    const realValueText = document.getElementById('real-value-text')
+    const currencyValueText = document.getElementById('currency-value-text')
 
-    console.log(inputReais / dolar)
+    // realValueText.innerHTML = inputReais;
+    realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
+        { style: 'currency', currency: 'BRL' }
+    ).format(inputReais);
+
+    currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
+        { style: 'currency', currency: 'USD' }
+    ).format(inputReais / dolar);
+
 }
 
-button.addEventListener ('click', convertValues)
+button.addEventListener('click', convertValues)
