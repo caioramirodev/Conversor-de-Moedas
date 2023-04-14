47af8ddf97ee5f1f6d3229c397bd2b7bf58f7703
caioramirodev <caioramirodev@gmail.com>
2023-04-13 21:51:04 -0300

    CommitJS3

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 
 const button = document.getElementById('convert-button')
+const select = document.getElementById('currency-select')
 
 const dolar = 5.2
 
@@ -9,15 +10,38 @@ const convertValues = () => {
     const realValueText = document.getElementById('real-value-text')
     const currencyValueText = document.getElementById('currency-value-text')
 
-    // realValueText.innerHTML = inputReais;
+    // realValueText.innerHTML = inputReais
     realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
         { style: 'currency', currency: 'BRL' }
-    ).format(inputReais);
+    ).format(inputReais)
 
     currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
         { style: 'currency', currency: 'USD' }
-    ).format(inputReais / dolar);
+    ).format(inputReais / dolar)
 
 }
 
+currencyChange = () =>{
+    const currencyName = document.getElementById ('currency-name')
+    const currencyImg = document.getElementById('currency-img')
+
+    if(select.value === 'US$ Dólar Americano'){
+        currencyName.innerHTML = "Dólar Americano"
+        currencyImg.src = "./assets/estados-unidos.png"
+    }
+   
+    if(select.value === '€ Euro'){
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "./assets/euro.png"
+    } 
+    
+    if(select.value === 'Bitcoin'){
+        currencyName.innerHTML = "Bitcoin"
+        currencyImg.src = "./assets/bitcoin.png"
+    }
+}
+
+
+
 button.addEventListener('click', convertValues)
+select.addEventListener('change', currencyChange)
